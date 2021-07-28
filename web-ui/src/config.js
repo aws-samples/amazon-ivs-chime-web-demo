9fa14528b06f313ac27be4d90e7397b3246ba000
Ayako Sayama <ayako@techsamurais.com>
2021-07-28 13:00:31 -0700

    fix config

--- web-ui/src/config.js
+++ web-ui/src/config.js
@@ -1,8 +1,7 @@
 export const BASE_HREF = '';
 
 // API endpoint for retrieving the attendees list, joining the room, and ending the room
-export const CHIME_ROOM_API =
-  'https://o70dtfufj7.execute-api.us-west-2.amazonaws.com/Prod';
+export const CHIME_ROOM_API = '';
 
 // Chime-SDK allows up to 16 attendee videos
 export const CHIME_ROOM_MAX_ATTENDEE = 16;
@@ -12,8 +11,7 @@ export const DEFAULT_VIDEO_STREAM =
   'https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.DmumNckWFTqz.m3u8';
 
 // Default Chat websocket link
-export const CHAT_WEBSOCKET =
-  'wss://0s2igshnic.execute-api.us-west-2.amazonaws.com/Prod';
+export const CHAT_WEBSOCKET = '';
 
 // Chime-SDK logging level: INFO, WARN, ERROR, DEBUG
 export const CHIME_LOG_LEVEL = 'WARN';
